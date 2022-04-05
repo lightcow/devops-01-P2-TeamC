5536eb0a51ad86504001ab08f3602ea3e3c6156e
seungyeoniii <alpcarmy@gmail.com>
2022-04-05 13:23:56 +0900

    update menu/delete.js

--- routes/api/menu/delete.js
+++ routes/api/menu/delete.js
@@ -1,7 +1,27 @@
+const { readOne, updateOne } = require("../../../model/restaurant.js")
 
 module.exports = async function (fastify, opts) {
-    fastify.delete('/', async function (request, reply) {
-      return 'this is an example'
+    fastify.delete('/:id', async function (request, reply) {
+      const restaurant = await readOne(this.mongo, '62468697e4f358304b6ec31d')
+      let newMenu=[]
+      for(let i = 0; i < restaurant.menu.length; i++){
+        if (!(restaurant.menu[i]._id.toString() === request.params.id)){
+          newMenu.push({...restaurant.menu[i]})
+        }
+      }
+      
+      // let wholeMenu = oldMenu;
+      // wholeMenu.push(newMenu);
+      
+      let newRestaurant= {menu: newMenu}
+      const result = await updateOne(this.mongo, '62468697e4f358304b6ec31d', newRestaurant)
+      // const modifiedRestaurant = await readOne(this.mongo, '62468697e4f358304b6ec31d')
+      
+      reply
+        .code(204)
+        .header('content-type', 'application/json')
+        .send('ok')
+      // return newRestaurant
     })
   }
   
